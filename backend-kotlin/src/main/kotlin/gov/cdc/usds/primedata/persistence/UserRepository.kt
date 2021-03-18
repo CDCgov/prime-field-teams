026f71ef31de2229602cbbf52fb8256023ede6a5
Carlos Felix <carlos.felix@agile6.com>
2021-03-18 10:27:51 -0400

    Removed sample files
    Removed sample files no longer needed

--- backend-kotlin/src/main/kotlin/gov/cdc/usds/primedata/persistence/UserRepository.kt
+++
@@ -1,6 +0,0 @@
-package gov.cdc.usds.primedata.persistence
-
-import gov.cdc.usds.primedata.model.User
-import org.springframework.data.repository.CrudRepository
-
-interface UserRepository : UserRepository<User, Long>
